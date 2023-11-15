4b0713c3b91a52b87e7dfb6651fe80d6249798a0
Dante Ramirez Lopez <kelvindanterl@gmail.com>
2023-11-15 15:38:16 -0500

    start project

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+       
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return user::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $user =  User::created([$request->all()]);
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
