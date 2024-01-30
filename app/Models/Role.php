d216f53cc3d48d299cb964c7cf39be82179bed5c
Dante Ramirez Lopez <kelvindanterl@gmail.com>
2024-01-30 10:24:13 -0500

    role plus

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,6 +9,8 @@ use  Illuminate\Database\Eloquent\Builder;
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name','guard_name','business_id'];
     protected static function booted()
     {
         static::addGlobalScope('only_bussines',function(Builder $builder){
